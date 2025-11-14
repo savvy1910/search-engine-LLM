95cff5d76ff82224f7e58fd9de019cd1dd953305
savvy1910 <savvy.techie.1910@gmail.com>
2025-11-14 07:23:02 +0530

    Integrate Tavily search and refactor imports
    Added Tavily search functionality and updated imports.

--- myApp.py
+++ myApp.py
@@ -1,11 +1,16 @@
 import streamlit as st
 from langchain_groq import ChatGroq
 from langchain_community.utilities import ArxivAPIWrapper,WikipediaAPIWrapper
-from langchain_community.tools import ArxivQueryRun,WikipediaQueryRun,DuckDuckGoSearchRun
+from langchain_community.tools.arxiv.tool import ArxivQueryRun
+from langchain_community.tools.wikipedia.tool import WikipediaQueryRun
+from langchain_community.tools.tavily_search import TavilySearchResults
 from langchain.agents import initialize_agent,AgentType
 from langchain.callbacks import StreamlitCallbackHandler
 import os
 from dotenv import load_dotenv
+
+load_dotenv()
+os.environ["TAVILY_API_KEY"] = "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"
 
 ## Arxiv and wikipedia Tools
 arxiv_wrapper=ArxivAPIWrapper(top_k_results=1, doc_content_chars_max=200)
@@ -14,16 +19,13 @@ arxiv=ArxivQueryRun(api_wrapper=arxiv_wrapper)
 api_wrapper=WikipediaAPIWrapper(top_k_results=1,doc_content_chars_max=200)
 wiki=WikipediaQueryRun(api_wrapper=api_wrapper)
 
-search=DuckDuckGoSearchRun(name="Search")
-
+search = TavilySearchResults(k=5)  # k = number of results
 
 st.title("🔎 LangChain - Chat with search")
 """
 In this example, we're using `StreamlitCallbackHandler` to display the thoughts and actions of an agent in an interactive Streamlit app.
 Try more LangChain 🤝 Streamlit Agent examples at [github.com/langchain-ai/streamlit-agent](https://github.com/langchain-ai/streamlit-agent).
 """
-
-# st.secrets["HF_TOKEN"]
 
 ## Sidebar for settings
 st.sidebar.title("Settings")
@@ -42,7 +44,7 @@ if prompt:=st.chat_input(placeholder="What is machine learning?"):
     st.chat_message("user").write(prompt)
 
     llm=ChatGroq(groq_api_key=api_key,model_name="qwen/qwen3-32b",streaming=True)
-    tools=[arxiv,wiki]
+    tools=[search,arxiv,wiki]
 
     search_agent=initialize_agent(tools,llm,agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,handle_parsing_errors=True)
 
